4e1bffc6c085ad8d4dfb4a7beab95cf10665ffb5
SebastienLuciani <sebastien.luciani@yahoo.com.mx>
2024-01-30 16:34:45 -0500

    Creation of needed files

---
+++ PortfolioWebsite/src/App.jsx
@@ -0,0 +1,21 @@
+import './css/App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+      <h1 className='text-green-500 text-center text-[11vw] '>SEBASTIEN LUCIANI</h1>
+    </>
+  )
+}
+
+export default App
